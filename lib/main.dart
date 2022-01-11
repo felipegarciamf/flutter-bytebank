4073839929d5b4c4f03ec09863de8f4c0aadd703
felipegarciamf <silasfelipegarcia@gmail.com>
2022-01-11 18:43:56 -0300

    extraindo método e refatorando o código, criando mclasses geneéricas

--- lib/main.dart
+++ lib/main.dart
@@ -84,55 +84,65 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: const TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: const InputDecoration(
-                labelText: "Número da conta",
-                hintText: "0000",
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              style: const TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: const InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: "Valor",
-                hintText: "0000",
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: "Número da Conta",
+              dica: "0000"),
+          Editor(
+              controlador: _controladorCampoValor,
+              rotulo: "Valor",
+              dica: "0.0",
+              icone: Icons.monetization_on),
           ElevatedButton(
-            onPressed: () {
-              debugPrint("clicou no confirmar!");
-
-              final int? numeroConta =
-                  int.tryParse(_controladorCampoNumeroConta.text);
-              final double? valor =
-                  double.tryParse(_controladorCampoValor.text);
-
-              if (numeroConta != null && valor != null) {
-                final transferenciaCriada = Transferencia(valor, numeroConta);
-                debugPrint('$transferenciaCriada');
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text('$transferenciaCriada'),
-                ));
-              }
-            },
+            onPressed: () => _criaTransferencia(context),
             child: Text("Confirmar"),
           ),
         ],
+      ),
+    );
+  }
+
+  void _criaTransferencia(BuildContext context) {
+     final int? numeroConta =
+        int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor =
+        double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      debugPrint('$transferenciaCriada');
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('$transferenciaCriada'),
+      ));
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController? controlador;
+
+  final String? rotulo;
+
+  final String? dica;
+
+  final IconData? icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: const TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
