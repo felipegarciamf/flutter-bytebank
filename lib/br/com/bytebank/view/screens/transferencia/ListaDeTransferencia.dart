18bbaaf7079bc505967c9c30c26e56469c7715e0
felipegarciamf <silasfelipegarcia@gmail.com>
2022-01-14 17:01:04 -0300

    corrigindo atualizacao de lista de transferencias

--- lib/br/com/bytebank/view/screens/transferencia/ListaDeTransferencia.dart
+++ lib/br/com/bytebank/view/screens/transferencia/ListaDeTransferencia.dart
@@ -5,38 +5,46 @@ import 'package:flutter/material.dart';
 import 'FormularioTransferencia.dart';
 import '../../widgets/ItemTransferencia.dart';
 
-class ListaTransferencias extends StatelessWidget {
+class ListaTransferencias extends StatefulWidget {
   final List<Transferencia> _transferencias = [];
 
   @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciasState();
+  }
+}
+
+class ListaTransferenciasState extends State<ListaTransferencias> {
+  @override
   Widget build(BuildContext context) {
-    _transferencias.add(Transferencia(1241.0, 124214));
     return Scaffold(
       appBar: AppBar(
-        title: Text('Transferencias'),
+        title: Text('Transferências'),
       ),
       body: ListView.builder(
-        itemCount: _transferencias.length,
+        itemCount: widget._transferencias.length,
         itemBuilder: (context, indice) {
-          final transferencia = _transferencias[indice];
+          final transferencia = widget._transferencias[indice];
           return ItemTransferencia(transferencia);
         },
       ),
       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
         onPressed: () {
           final Future future =
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
             return FormularioTransferencia();
           }));
           future.then((transferenciaRecebida) {
-            debugPrint("chegou no then do future");
-            debugPrint("$transferenciaRecebida");
-            _transferencias.add(transferenciaRecebida);
+            Future.delayed(Duration(seconds: 1), () {
+              if (transferenciaRecebida != null) {
+                setState(() {
+                  widget._transferencias.add(transferenciaRecebida);
+                });
+              }
+            });
           });
-
-          debugPrint("entrou aqui");
         },
-        child: const Icon(Icons.add),
       ),
     );
   }
