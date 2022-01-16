fce2dffdff9bd77f0850958d704ae84dc7ad1d11
felipegarciamf <silasfelipegarcia@gmail.com>
2022-01-15 23:06:06 -0300

    melhorando experiencia e fluxo do app

--- lib/br/com/bytebank/main.dart
+++ lib/br/com/bytebank/main.dart
@@ -1,6 +1,7 @@
 import 'package:bytebank/br/com/bytebank/view/screens/cadastroUsuario/FormularioCadastroView.dart';
 import 'package:bytebank/br/com/bytebank/view/screens/checkin/CheckinView.dart';
 import 'package:bytebank/br/com/bytebank/view/screens/checkin/ListaDeCheckinView.dart';
+import 'package:bytebank/br/com/bytebank/view/screens/home/HomeView.dart';
 import 'package:bytebank/br/com/bytebank/view/screens/login/LoginView.dart';
 import 'package:bytebank/br/com/bytebank/view/screens/transferencia/FormularioTransferenciaView.dart';
 import 'package:bytebank/br/com/bytebank/viewmodel/login/LoginViewModel.dart';
@@ -13,66 +14,9 @@ void main() => runApp(
 class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: HomeView(),
-      ),
-    );
-  }
-}
-
-
-class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static  final List<Widget> _widgetOptions = <Widget>[
-    CheckinView()
-    ,
-    ListaDeCheckinView(),
-    const Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        body: LoginView(),
       ),
     );
   }
